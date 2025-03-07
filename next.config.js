0e6d1ce0145dceb3a0ee84f5898ce8e456af869d
Gradin Gheti <gradin_gheti@netex.ro>
2025-03-07 17:13:54 +0200

    modify menu and some sections

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     remotePatterns: [
       {
@@ -12,6 +13,6 @@ const nextConfig = {
       },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,35 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Next.js App',
-  description: 'Created with Next.js',
+  title: 'Olga Nielsen | Tech Lawyer & Data Compliance Expert',
+  description:
+    'lga Nielsen is a tech lawyer with 10+ years of experience in GDPR, AI Act, NIS2, and data compliance. She specializes in AI, cybersecurity, and digital regulations.',
+  keywords: [
+    'Olga Nielsen',
+    'Tech Lawyer',
+    'Data Protection',
+    'GDPR Expert',
+    'AI Act',
+    'NIS2',
+    'Cybersecurity',
+    'Data Compliance',
+    'Digital Law',
+    'Information Security',
+    'DPO as a Service',
+  ].join(', '),
+  authors: [{ name: 'Olga Nielsen', url: 'https://olganielsen.com' }],
+  openGraph: {
+    title: 'Olga Nielsen | Tech Lawyer & Data Compliance Expert',
+    description:
+      'Tech lawyer with expertise in GDPR, AI, cybersecurity, and data compliance. Helping businesses navigate EU digital regulations.',
+    url: 'https://olganielsen.com',
+    siteName: 'Olga Nielsen - Tech Lawyer',
+    type: 'profile',
+  },
+
+  alternates: {
+    canonical: 'https://olganielsen.com',
+  },
 };
 
 export default function RootLayout({
